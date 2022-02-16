c9b5073abe7a20bd60e04e5ed184ca93910bac02
RetepMil <lkslyj2@naver.com>
2022-02-16 20:29:42 +0900

    feature: login check middleware

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -7,11 +7,20 @@ import {
   HeartIcon,
   RssIcon,
 } from '@heroicons/react/outline'
+import { signOut, useSession } from 'next-auth/react'
 
 function Sidebar() {
+  const { data: session, status } = useSession()
+
   return (
     <div className="border-r border-gray-900 p-5 text-sm text-gray-500">
       <div className="space-y-4">
+        <button
+          className="flex items-center space-x-2 hover:text-white"
+          onClick={() => signOut()}
+        >
+          <p>Logout</p>
+        </button>
         <button className="flex items-center space-x-2 hover:text-white">
           <HomeIcon className="h-5 w-5" />
           <p>Home</p>
